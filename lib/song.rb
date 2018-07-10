1c7fb000a0f144ac87e03131b580ab6426b5dcbb
Jeremy Parker <jrobinparker@gmail.com>
2018-07-10 03:42:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,8 @@ class Song
     Hash[*@@genres.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
   end
   
-  def self.genre_count
-    Hash[*@@genres.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+  def self.artist_count
+    Hash[*@@artists.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
   end
   
 end 
