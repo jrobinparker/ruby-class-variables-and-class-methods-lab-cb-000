45901308885d936b45b5acd95412ef4e50960533
Jeremy Parker <jrobinparker@gmail.com>
2018-07-10 03:35:53 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  attr_reader :name, :artist, :genre
+  
+  @@count = 0 
+  @@artists = [] 
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist
+  end 
+  
+  def self.count
+    @@count 
+  end
+  
+  def self.genres
+    @@genres.uniq 
+  end 
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    genre_array = @@genres.split("\n")
+    gerne
+    genre_array.each do |genre|
+      
+      
+      genre_hash.merge(genre)
+    end
+
+    genre_hash
+  end
+  
+end 
